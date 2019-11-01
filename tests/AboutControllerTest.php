8568d3b91c7bf5473e7bf8e534dbd50dc182a3e2
Natalia Stanko <anithaly@gmail.com>
2019-11-01 22:39:14 +0000

    add first functional test

---
+++ tests/AboutControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AboutControllerTest extends WebTestCase
+{
+    public function testAboutIndexList()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/about/');
+
+        $this->assertResponseIsSuccessful();
+        // $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        // $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
